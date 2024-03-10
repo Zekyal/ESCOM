e4a857e1a009ad381bece157fe1324ccb6d39027
Mauro Sampayo <maurosampayo64@gmail.com>
2024-03-10 00:12:00 -0600

    Delete DataBase_Selected_Topics directory

--- DataBase_Selected_Topics/Proyecto/EQ4-Hospital/HTML/scripts/confirmar_borrado.js
+++
@@ -1,9 +0,0 @@
-function Confirmar_Borrado(ID) {
-    var respuesta = confirm("¿Desea borrar el registro con ID: " + ID + "?");
-
-    if (respuesta == true) {
-        return true;
-    } else {
-        return false;
-    }
-}
